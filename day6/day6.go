508ba704b12f418bc9f7cb880765ee2d0de22a23
Joel Abshier <jabshier@influxdata.com>
2022-12-06 01:35:43 -0600

    🎄 Day 6: Tuning Trouble - Part 2

--- day6/day6.go
+++ day6/day6.go
@@ -8,20 +8,30 @@ import (
 
 const filename = "datastream.txt"
 
+type Queue struct {
+	Queue  []string
+	MaxLen int
+}
+
 func main() {
 	lines := utils.ReadLines(filename)
-	// lines := sampleBuffer()
 
 	for _, buffer := range lines {
-		startPos := findPacketStart(buffer)
-		fmt.Printf("Found start of packet at %d\n", startPos)
+		// Part 1
+		packetStartPos := findPacketStart(buffer, 4)
+		fmt.Printf("Found start of packet at %d\n", packetStartPos)
+
+		// Part 2
+		messageStartPos := findPacketStart(buffer, 14)
+		fmt.Printf("Found start of message at %d\n", messageStartPos)
 	}
 }
 
-func findPacketStart(buffer string) int {
-	var queue []string
+func findPacketStart(buffer string, distinctChars int) int {
+	queue := Queue{Queue: []string{}, MaxLen: distinctChars}
+
 	for ind, char := range buffer {
-		if len(queue) == 4 && isUniqueList(queue) {
+		if len(queue.Queue) == distinctChars && isUniqueList(queue.Queue) {
 			return ind
 		}
 
@@ -41,19 +51,10 @@ func isUniqueList(list []string) bool {
 	return len(distinct) == len(list)
 }
 
-func pushIn(queue []string, value string) []string {
-	if len(queue) == 4 {
-		queue = queue[1:]
+func pushIn(queue Queue, value string) Queue {
+	if len(queue.Queue) == queue.MaxLen {
+		queue.Queue = queue.Queue[1:]
 	}
-	return append(queue, value)
-}
-
-func sampleBuffer() []string {
-	return []string{
-		"mjqjpqmgbljsphdztnvjfqwrcgsmlb",
-		"bvwbjplbgvbhsrlpgdmjqwftvncz",
-		"nppdvjthqldpwncqszvftbrmjlhg",
-		"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
-		"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
-	}
+	queue.Queue = append(queue.Queue, value)
+	return queue
 }
