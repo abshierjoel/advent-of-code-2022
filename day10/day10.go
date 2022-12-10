3be51c8d3f88fa439346ce1384b76e9e8c51ff7e
Joel Abshier <jabshier@influxdata.com>
2022-12-10 03:04:19 -0600

    🎄 Day 10: Cathode-Ray Tube - Part B
    I was able to get the right answer from part B, but there's a problem with the cycle counts from 0..39 rather than 1..40. Everything is off b 1 in either the math or the grid, and I need to adjust it

--- day10/day10.go
+++ day10/day10.go
@@ -21,7 +21,7 @@ func main() {
 	instructions := utils.ReadLines(filename)
 	signalStrength := cpu.getSignalStrengthSum(instructions)
 
-	fmt.Printf("Signal Strength: %d\n", signalStrength)
+	fmt.Printf("\n\nSignal Strength: %d\n", signalStrength)
 }
 
 func (cpu *CPU) getSignalStrengthSum(instructions []string) int {
@@ -44,15 +44,33 @@ func (cpu *CPU) addx(addend_str string) {
 
 	addend, _ := strconv.Atoi(addend_str)
 	cpu.X += addend
+	cpu.Cycles++
 
-	cpu.noop()
+	cpu.cycle(addend)
 }
 
 func (cpu *CPU) noop() {
 	cpu.Cycles++
-	if (cpu.Cycles%40)-20 == 0 {
-		cpu.Total += cpu.Cycles * cpu.X
+	cpu.cycle(0)
+}
+
+func (cpu *CPU) cycle(add int) {
+	xPosition := (cpu.Cycles % 40)
+	if xPosition >= cpu.X-1 && xPosition <= cpu.X+1 {
+		fmt.Printf("#")
+	} else {
+		fmt.Printf(".")
+	}
+
+	if (cpu.Cycles % 40) == 0 {
+		fmt.Printf("\n")
+	}
+
+	if ((cpu.Cycles+1)%40)-20 == 0 {
+		cpu.Total += (cpu.Cycles + 1) * cpu.X
 	}
 }
 
-func newCpu() CPU { return CPU{X: 1, Cycles: 1} }
+func newCpu() CPU { return CPU{X: 1, Cycles: 0} }
+
+// RUAKHBEK
