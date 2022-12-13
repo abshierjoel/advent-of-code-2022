c5f603ae0b7a0c57cf0fd44e22da2eb72f475d95
Joel Abshier <jabshier@influxdata.com>
2022-12-13 14:06:20 -0600

    🎄Day 13: Distress Signal - A distressing pile of code

--- day13/day13.go
+++ day13/day13.go
@@ -1,5 +1,159 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sort"
+	"strconv"
 
+	"github.com/abshierjoel/advent-of-code-2022/utils"
+)
+
+const filename = "packets.txt"
+
+type Packet struct {
+	Value    int
+	Elements []*Packet
+	Parent   *Packet
+}
+
+type Pair struct {
+	PacketA *Packet
+	PacketB *Packet
+}
+
+func main() {
+	lines := utils.ReadLines(filename)
+	pairs := parsePackets(lines)
+
+	// Part 1
+	printOrderedIndexSum(pairs)
+
+	// Part 2
+	var allPackets []*Packet
+	for _, pair := range pairs {
+		allPackets = append(allPackets, pair.PacketA)
+		allPackets = append(allPackets, pair.PacketB)
+	}
+
+	allPackets = append(allPackets, parsePacket("[[2]]"))
+	allPackets = append(allPackets, parsePacket("[[6]]"))
+
+	sort.Slice(allPackets, func(i, j int) bool {
+		return isInOrder(allPackets[i], allPackets[j]) == 1
+	})
+
+	key := 1
+	for i, packet := range allPackets {
+		fmt.Println(packet)
+		if isInOrder(packet, parsePacket("[[2]]")) == 0 || isInOrder(packet, parsePacket("[[6]]")) == 0 {
+			fmt.Println(i)
+			key *= i + 1
+		}
+	}
+
+	fmt.Println(key)
+}
+
+func printOrderedIndexSum(pairs []Pair) {
+	var results []int
+	for i, pair := range pairs {
+		if isInOrder(pair.PacketA, pair.PacketB) >= 0 {
+			results = append(results, i+1)
+		}
+	}
+
+	var sum int
+	for _, v := range results {
+		sum = sum + v
+	}
+
+	fmt.Println("Sum of Ordered Packet Indices")
+	fmt.Println(sum)
+}
+
+func isInOrder(a *Packet, b *Packet) int {
+	switch {
+	case len(a.Elements) == 0 && len(b.Elements) == 0:
+		if a.Value < b.Value {
+			return 1
+		} else if a.Value == b.Value {
+			return 0
+		} else {
+			return -1
+		}
+	case a.Value >= 0:
+		return isInOrder(&Packet{-1, []*Packet{a}, nil}, b)
+	case b.Value >= 0:
+		return isInOrder(a, &Packet{-1, []*Packet{b}, nil})
+	default:
+		var i int
+		for i = 0; i < len(a.Elements) && i < len(b.Elements); i++ {
+			order := isInOrder(a.Elements[i], b.Elements[i])
+			if order != 0 {
+				return order
+			}
+		}
+		if i < len(a.Elements) {
+			return -1
+		} else if i < len(b.Elements) {
+			return 1
+		}
+	}
+
+	return 0
+}
+
+func parsePackets(lines []string) []Pair {
+	var pairs []Pair
+	var one *Packet
+	var two *Packet
+
+	for _, line := range lines {
+		if line == "" {
+			pairs = append(pairs, Pair{one, two})
+			one, two = nil, nil
+		} else if one == nil {
+			one = parsePacket(line)
+		} else if two == nil {
+			two = parsePacket(line)
+		}
+	}
+
+	return pairs
+}
+
+func parsePacket(line string) *Packet {
+	packet := Packet{Value: -1, Elements: []*Packet{}, Parent: nil}
+	currentParent := &packet
+
+	var numStr string
+	for _, c := range line {
+		switch c {
+		case '[':
+			node := Packet{-1, []*Packet{}, currentParent}
+			currentParent.Elements = append(currentParent.Elements, &node)
+			currentParent = &node
+		case ']':
+			if len(numStr) > 0 {
+				number, _ := strconv.Atoi(numStr)
+				currentParent.Value = number
+				numStr = ""
+			}
+			currentParent = currentParent.Parent
+		case ',':
+			if len(numStr) > 0 {
+				number, _ := strconv.Atoi(numStr)
+				currentParent.Value = number
+				numStr = ""
+			}
+			currentParent = currentParent.Parent
+			node := Packet{-1, []*Packet{}, currentParent}
+			currentParent.Elements = append(currentParent.Elements, &node)
+			currentParent = &node
+		default:
+			numStr += string(c)
+		}
+	}
+
+	return &packet
 }
--- day13/day13_test.go
+++ day13/day13_test.go
@@ -1,7 +1,107 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestFunc(t *testing.T)  {
+	"github.com/stretchr/testify/assert"
+)
 
+func TestIsInOrderReturnsPositioning(t *testing.T) {
+	pairs := []Pair{
+		{
+			PacketA: &Packet{-1, []*Packet{{Value: 5}}, nil},
+			PacketB: &Packet{-1, []*Packet{{Value: 10}}, nil},
+		},
+		{
+			PacketA: &Packet{10, []*Packet{}, nil},
+			PacketB: &Packet{20, []*Packet{}, nil},
+		}}
+
+	var results []int
+	for i, pair := range pairs {
+		if isInOrder(pair.PacketA, pair.PacketB) >= 0 {
+			results = append(results, i)
+		}
+	}
+
+	expected := []int{0, 1}
+	assert.Equal(t, expected, results)
+
+}
+
+func TestIsInOrder(t *testing.T) {
+	pairs := parsePackets(sample())
+
+	var results []int
+	for i, pair := range pairs {
+		if isInOrder(pair.PacketA, pair.PacketB) >= 0 {
+			results = append(results, i+1)
+		}
+	}
+
+	expected := []int{1, 2, 4, 6}
+	assert.Equal(t, expected, results)
+
+}
+
+func TestParsePackets(t *testing.T) {
+	result := parsePackets(sample())
+
+	assert.Equal(t, 7, len(result))
+}
+
+func TestParsePacket(t *testing.T) {
+	input := "[1,2,3]"
+	result := parsePacket(input)
+
+	assert.Equal(t, 1, result.Elements[0].Value)
+	assert.Equal(t, 2, result.Elements[1].Value)
+	assert.Equal(t, 3, result.Elements[2].Value)
+}
+func TestParseDeepPacket(t *testing.T) {
+	input := "[1,[2,[3,[4,[5,6,7]]]],8,9]"
+	result := parsePacket(input)
+
+	inner1 := result.Elements[1]
+	inner2 := inner1.Elements[1]
+	inner3 := inner2.Elements[1]
+	inner4 := inner3.Elements[1]
+
+	assert.Equal(t, 1, result.Elements[0].Value)
+	assert.Equal(t, 2, inner1.Elements[0].Value)
+	assert.Equal(t, 3, inner2.Elements[0].Value)
+	assert.Equal(t, 4, inner3.Elements[0].Value)
+	assert.Equal(t, 5, inner4.Elements[0].Value)
+	assert.Equal(t, 6, inner4.Elements[1].Value)
+	assert.Equal(t, 7, inner4.Elements[2].Value)
+	assert.Equal(t, 8, result.Elements[2].Value)
+	assert.Equal(t, 9, result.Elements[3].Value)
+}
+
+func sample() []string {
+	return []string{
+		"[1,1,3,1,1]",
+		"[1,1,5,1,1]",
+		"",
+		"[[1],[2,3,4]]",
+		"[[1],4]",
+		"",
+		"[9]",
+		"[[8,7,6]]",
+		"",
+		"[[4,4],4,4]",
+		"[[4,4],4,4,4]",
+		"",
+		"[7,7,7,7]",
+		"[7,7,7]",
+		"",
+		"[]",
+		"[3]",
+		"",
+		"[[[]]]",
+		"[[]]",
+		"",
+		"[1,[2,[3,[4,[5,6,7]]]],8,9]",
+		"[1,[2,[3,[4,[5,6,0]]]],8,9]",
+	}
 }
